369e3da9b0ff40b06e5a9c8e11b1743942582994
Webdevmaksim <maksim.saraev.121.5@gmail.com>
2020-07-26 17:12:33 +0300

    Feat:fancybox + button up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  //slider
     var mySwiper = new Swiper('.sw-slider__container', {
         // Optional parameters
         loop: true,
@@ -15,4 +16,25 @@ $(document).ready(function () {
         var prev = $('.sw-slider__button-prev');
         // Margin-positioning for the left arrow 
         next.css('left', prev.width() + 15);
+      //Lazy loading
+      $(function(){
+        $('.lazy').lazy();
+      });
+      //Кнопка прокрутки наверх
+      $(window).scroll(function () {
+        // Если отступ сверху больше 50px то показываем кнопку "Наверх"
+        if ($(this).scrollTop() > 50) {
+            $('.button-up').fadeIn();
+        } else {
+            $('.button-up').fadeOut();
+        }
+    });
+    
+    /** При нажатии на кнопку мы перемещаемся к началу страницы */
+    $('.button-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
 });
