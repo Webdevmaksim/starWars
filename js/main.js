1f4f18c5428d3047e9983925f58ce1cace6a7d8f
Webdevmaksim <maksim.saraev.121.5@gmail.com>
2020-07-26 11:31:55 +0300

    slider process

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    var mySwiper = new Swiper('.sw-slider__container', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 4,
+        // Navigation arrows
+        navigation: {
+          nextEl: '.sw-slider__button-next',
+          prevEl: '.sw-slider__button-prev',
+        },
+      });
+      //arrows position
+        var next = $('.sw-slider__button-next');
+        var prev = $('.sw-slider__button-prev');
+        // Margin-positioning for the left arrow 
+        next.css('left', prev.width() + 15);
+});
